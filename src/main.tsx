651f5687209ae770526a8f43496e0e80b276cc57
Thomas <thomas.bouasli@gmail.com>
2023-08-15 15:55:41 -0300

    added analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
@@ -10,5 +11,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <Router />
         <Toaster />
+        <Analytics />
     </React.StrictMode>,
 );
