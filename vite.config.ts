705577c34d36f5088abdff1dbc18597c1e8eb304
Thomas <thomas.bouasli@gmail.com>
2023-08-15 15:24:09 -0300

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,50 @@
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa';
+
+const manifestForPlugin: Partial<VitePWAOptions> = {
+    registerType: 'prompt',
+    includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+    manifest: {
+        name: 'Chat Box',
+        short_name: 'Chat Box',
+        description: 'An app to chat with your friends',
+        icons: [
+            {
+                src: '/android-chrome-192x192.png',
+                sizes: '192x192',
+                type: 'image/png',
+            },
+            {
+                src: '/android-chrome-512x512.png',
+                sizes: '512x512',
+                type: 'image/png',
+            },
+            {
+                src: '/apple-touch-icon.png',
+                sizes: '180x180',
+                type: 'image/png',
+                purpose: 'apple touch icon',
+            },
+            {
+                src: '/maskable_icon.png',
+                sizes: '225x225',
+                type: 'image/png',
+                purpose: 'any maskable',
+            },
+        ],
+        theme_color: '#171717',
+        background_color: '#e8ebf2',
+        display: 'standalone',
+        scope: '/',
+        start_url: '/',
+        orientation: 'portrait',
+    },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), VitePWA(manifestForPlugin)],
     resolve: {
         alias: {
             '@': '/src',
